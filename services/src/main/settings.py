8a5d7b4bae6ad6848aa60413f0c41184a62c5b05
Fethullah Misir <fethullah.misir@hotmail.de>
2020-09-18 23:58:14 +0000

    update conf

--- services/src/main/settings.py
+++ services/src/main/settings.py
@@ -156,6 +156,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-#USE_X_FORWARDED_HOST = True
+USE_X_FORWARDED_HOST = True
 STATIC_URL = '/static/'
 STATIC_ROOT= os.path.join(BASE_DIR, "static")
