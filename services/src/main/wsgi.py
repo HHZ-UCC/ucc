844e7186a00a602a785d977b5029d190f87374f7
Fethullah Misir <fethullah.misir@hotmail.de>
2020-09-19 18:02:28 +0000

    Add static file loading

--- services/src/main/wsgi.py
+++ services/src/main/wsgi.py
@@ -10,7 +10,10 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
+from django.conf import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')
 
 application = get_wsgi_application()
+application = WhiteNoise(application, root=settings.STATIC_ROOT)
