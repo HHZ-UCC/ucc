80e2daaa545e08dbc6866d65515b01c6cc12a654
Fethullah Misir <fethullah.misir@hotmail.de>
2020-11-27 22:50:21 +0000

    Add devcontainer configurations

--- services/src/main/settings.py
+++ services/src/main/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
@@ -164,6 +163,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 USE_X_FORWARDED_HOST = True
-FORCE_SCRIPT_NAME = '/services'
-STATIC_URL = FORCE_SCRIPT_NAME + '/static/'
+
+if DEBUG:
+    STATIC_URL = '/static/'
+else:
+    FORCE_SCRIPT_NAME = '/services'
+    STATIC_URL = FORCE_SCRIPT_NAME + '/static/'
+
 STATIC_ROOT= os.path.join(BASE_DIR, "static")
